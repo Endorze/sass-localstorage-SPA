dd1890b3f4da5dc89070c626a28592166e5dbd34
Endorze <alexyurihallgren@hotmail.com>
2025-05-13 16:41:45 +0200

    feat: Made website responsive, added data file, added carousel.

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,7 @@
 import type { FC } from "react";
 import Button from "../Button/Button";
+import styles from "./Dashboard.module.scss";
+import ImageCarousel from "../Carousel/Carousel";
 
 type Props = {
   onLogout: () => void;
@@ -7,16 +9,20 @@ type Props = {
 
 const Dashboard:FC<Props> = ({ onLogout }) => {
   const stored = localStorage.getItem('account');
-  const username = stored ? JSON.parse(stored).username : 'okänd';
+  const username = stored ? JSON.parse(stored).username : 'unknown';
 
   return (
-    <div>
-      <h2>Välkommen, {username}!</h2>
-      <Button onClick={() => {
-        localStorage.setItem("isLoggedIn", "false");
-        onLogout();
-      }} text="Log Out"/>
-      
+    <div className={styles.dashboard}>
+      <div className={styles.dashboardHeader}>
+        <h2>Welcome, {username}!</h2>
+        <Button onClick={() => {
+          localStorage.setItem("isLoggedIn", "false");
+          onLogout();
+        }} text="Log Out"/>
+      </div>
+      <div>
+        <ImageCarousel />
+      </div>
     </div>
   );
 };
